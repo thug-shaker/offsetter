05dd73c4a59db37241b3b32450ff4b152304c74e
Jim Colerick <pastorjim@anal.repair>
2023-01-21 12:23:17 +0100

    Added an export macro that does the same as debug but makes everything public except padding

--- src/lib.rs
+++ src/lib.rs
@@ -52,3 +52,33 @@ macro_rules! offset_debug {
         offset_debug!(@guard (0, $($input)*) -> {$(#[$attr])* struct $struct_name});
     };
 }
+
+#[macro_export]
+macro_rules! offset_export {
+    // Guarded munched exprs
+    // output
+    (@guard ($current_offset:expr,) -> {$(#[$attr:meta])* struct $name:ident $(($amount:expr, $id:ident: $ty:ty))*}) => {
+
+        $crate::paste::paste! {
+            #[repr(C, packed)]
+            $(#[$attr])* pub struct $name { $([<_pad $id>]: [u8;$amount], pub $id: $ty),* }
+        }
+        
+        impl core::fmt::Debug for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.debug_struct(stringify!($name))
+                 $(.field(stringify!($id), unsafe { &core::ptr::read_unaligned(core::ptr::addr_of!(self.$id))}))*
+                 .finish()
+            }
+        }
+    };
+    // add more fields
+    (@guard ($current_offset:expr, $offset:literal $id:ident: $ty:ty, $($next:tt)*) -> {$($output:tt)*}) => {
+        offset_export!(@guard ($offset + core::mem::size_of::<$ty>(), $($next)*) -> {$($output)* ($offset - ($current_offset), $id: $ty)});
+    };
+
+    // Entry points
+    ($(#[$attr:meta])* struct $struct_name:ident {$($input:tt)*}) => {
+        offset_export!(@guard (0, $($input)*) -> {$(#[$attr])* struct $struct_name});
+    };
+}
